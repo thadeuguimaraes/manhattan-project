c2655c59d5844597cf624a30bb092cad65dec7cb
Arthur Passos <arthur.opassos@gmail.com>
2023-09-04 11:56:44 -0300

    Desafio Técnico

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import http from 'http';
+import PG from 'pg';
+
+const port = Number(process.env.port);
+
+const client = new PG.Client(
+  `postgres://${user}:${pass}@${host}:${db_port}`
+);
+
+let successfulConnection = false;
+
+http.createServer(async (req, res) => {
+  console.log(`Request: ${req.url}`);
+
+  if (req.url === "/api") {
+    client.connect()
+      .then(() => { successfulConnection = true })
+      .catch(err => console.error('Database not connected -', err.stack));
+
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+
+    let result;
+
+    try {
+      result = (await client.query("SELECT * FROM users")).rows[0];
+    } catch (error) {
+      console.error(error)
+    }
+
+    const data = {
+      database: successfulConnection,
+      userAdmin: result?.role === "admin"
+    }
+
+    res.end(JSON.stringify(data));
+  } else {
+    res.writeHead(503);
+    res.end("Internal Server Error");
+  }
+
+}).listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
