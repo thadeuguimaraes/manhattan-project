9665cc368868fa854fd0012d0eda5da83f6f93fa
Thadeu Guimarães <52017205+thadeuguimaraes@users.noreply.github.com>
2023-09-06 22:40:38 -0300

    feat: projeto release:v1.0.0

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "POSTGRES_USER" {
+  default = "cubos" # Valor padrão para POSTGRES_USER
+}
+variable "POSTGRES_PW" {
+  default = "cubos_net" # Valor padrão para POSTGRES_PW
+}
+variable "POSTGRES_DB" {
+  default = "postgres" # Valor padrão para POSTGRES_DB
+}
+variable "PGADMIN_MAIL" {
+  default = "marciothadeu1984@gmail.com" # Valor padrão para PGADMIN_MAIL
+}
+variable "PGADMIN_PW" {
+  default = "canes123" # Valor padrão para PGADMIN_PW
+}
